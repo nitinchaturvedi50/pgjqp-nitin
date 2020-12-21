7f6625951c249b3dafa4208b27ed2a0fff1402f0
nitinchaturvedi50 <74126521+nitinchaturvedi50@users.noreply.github.com>
2020-12-21 23:21:46 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Subtract3
+{  
+   public static void main(String[] args)
+   {
+    int num1,num2;
+    System.out.println("enter the two numbers");
+    Scanner sc=new Scanner(System.in);
+    num1=sc.nextInt();
+    num2=sc.nextInt();
+  System.out.println(num1-num2);
+}
+}
+
+//in this error:C:\Users\nitin\OneDrive\Desktop\java codes\javacodes>java add
+Error: Could not find or load main class add
+Caused by: java.lang.ClassNotFoundException: add
