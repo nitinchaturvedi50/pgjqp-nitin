02a144f0830f163f2394b02ac6e42b253ac61ba5
nitinchaturvedi50 <74126521+nitinchaturvedi50@users.noreply.github.com>
2020-12-28 16:28:59 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,17 @@
+class Datatype
+
+{
+  public static void main(String[] args)
+ {
+   int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+ System.out.println(("Size of int: %zu bytes\n",(sizeof(intType)));
+System.out.println(("Size of float: %zu bytes\n",( sizeof(floatType)));
+System.out.println(("Size of double: %zu bytes\n",(sizeof(doubleType)));
+System.out.println("Size of char: %zu bytes\n",(sizeof(charType)));
+
+
+}
+}
